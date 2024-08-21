c5f6751495faba47ab42b3bc583c2c7961b715fb
B.G.V.Shiva <118362268+bgvshiva2004@users.noreply.github.com>
2024-08-21 19:44:38 +0530

    upload

---
+++ bridge/app/page.tsx
@@ -0,0 +1,12 @@
+import BgWrapper from "@/components/ListWrapper";
+
+
+export default function Page() {
+  return(
+    <div>
+      <div className="-mt-[3%]">
+        <BgWrapper/>
+      </div>
+    </div>
+  )
+}
---
+++ bridge/app/profile/page.tsx
@@ -0,0 +1,8 @@
+
+export default function page() {
+  return (
+    <div>
+        profile page
+    </div>
+  )
+}
